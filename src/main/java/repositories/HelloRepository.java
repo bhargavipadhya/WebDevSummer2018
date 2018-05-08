0fdc071212d3ad3b63e8b39627494e38494700a1
Bhargavi Padhya <bhargavi95@Bhargavis-MacBook-Pro.local>
2018-05-08 17:17:01 -0400

    Created repo, service and model

---
+++ src/main/java/repositories/HelloRepository.java
@@ -0,0 +1,8 @@
+package repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import model.Hello;
+
+public interface HelloRepository
+extends CrudRepository<Hello, Integer> {}
